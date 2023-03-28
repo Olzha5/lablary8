7a5cf4c5e138929b0200f9f196c675e95e4c7dd5
Nurgali <121267768+Nurgali01@users.noreply.github.com>
2023-03-28 08:41:26 +0600

    Update nur.py

--- nur.py
+++ nur.py
@@ -1,2 +1,8 @@
 print("Hello Daulet agai")
 Print("tagy da code")
+
+import math 
+x=float(input("x = "));
+y=float(input("y = "));
+Z=(((math.log(x+y)-math.fabs(x-5*y))/(math.sin(5.3*x)+(2*x)/(math.sqrt(math.fabs(5*y)))))-((math.cos(y)**3)/(x+y)));
+print("Z = ",Z)
