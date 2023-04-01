3615610d9f748a7959abc9efad1df466f0523797
Olzhas Abukhan <94923512+Olzha5@users.noreply.github.com>
2023-04-01 14:32:10 +0600

    Update nur.py

--- nur.py
+++ nur.py
@@ -6,3 +6,5 @@ x=float(input("x = "));
 y=float(input("y = "));
 Z=(((math.log(x+y)-math.fabs(x-5*y))/(math.sin(5.3*x)+(2*x)/(math.sqrt(math.fabs(5*y)))))-((math.cos(y)**3)/(x+y)));
 print("Z = ",Z)
+
+print("Abylai")
